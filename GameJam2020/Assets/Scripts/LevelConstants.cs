afee5cfa3ad56b169b011abf622cf5beb5a1de56
mrac <rac.michalmail@gmail.com>
2020-01-31 23:14:59 +0100

    Setup basic level and pieces

---
+++ GameJam2020/Assets/Scripts/LevelConstants.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LevelConstants
+{
+    public static readonly int PIECE_SIZE = 8;
+    public static readonly int LEVEL_WIDTH = 30;
+    public static readonly int LEVEL_HEIGHT = 24;
+}
---
+++ GameJam2020/Assets/Scripts/LevelSetupBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSetupBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject bg;
+
+
+    private void Awake()
+    {
+        ResizeBgToScreen();
+    }
+
+    private void ResizeBgToScreen()
+    {
+        var sr = bg.GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        transform.localScale = new Vector3(1, 1, 1);
+
+        var width = sr.sprite.bounds.size.x;
+        var height = sr.sprite.bounds.size.y;
+
+        var worldScreenHeight = Camera.main.orthographicSize * 2.0;
+        var worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        var targetScale = new Vector3((float)worldScreenWidth / width, 1, (float)worldScreenHeight / height);
+        bg.transform.localScale = targetScale;
+    }
+}
