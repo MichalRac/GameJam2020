0ef8d5ea4d859fdcdd68704277428b05455f1758
Mariusz Zdunek <Mariusz.Zdunek.it@gmail.com>
2020-02-01 14:11:02 +0100

    BLocks movement

--- GameJam2020/Assets/Scripts/TetrominosBehaviour.cs
+++ GameJam2020/Assets/Scripts/TetrominosBehaviour.cs
@@ -5,4 +5,34 @@ using UnityEngine;
 public class TetrominosBehaviour : MonoBehaviour
 {
     [SerializeField] List<TetrominosPiece> tetrominosPieces;
+
+    public float UpdateTimeInterval = 1f; //in seconds
+    public float Distance = 8f;
+    private float currentTime;
+
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        UpdateTetrominos();
+    }
+
+
+    private void UpdateTetrominos()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime > UpdateTimeInterval)
+        {
+            currentTime = 0f;
+            MoveTetrominos();
+        }
+    }
+
+    private void MoveTetrominos()
+    {
+        transform.Translate(new Vector3(0,-Distance,0f));
+    }
 }
--- GameJam2020/Assets/Scripts/TetrominosPiece.cs
+++ GameJam2020/Assets/Scripts/TetrominosPiece.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class TetrominosPiece : MonoBehaviour
 {
-    
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log($"OnCollisionEnter2D with {other.transform.name} ");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log($"OnTriggerEnter2D with {other.transform.name} ");
+    }
 }
