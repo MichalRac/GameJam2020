c63469e773a95e6b80834a1ee65f299319eba644
mrac <rac.michalmail@gmail.com>
2020-02-01 18:38:34 +0100

    Add basic snapping action

--- GameJam2020/Assets/Scripts/PlayerMovement.cs
+++ GameJam2020/Assets/Scripts/PlayerMovement.cs
@@ -44,6 +44,20 @@ public class PlayerMovement : MonoBehaviour
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
 
+        int directionModifier = horizontal > 0 ? 1 : -1;
+
+        if (Input.GetButton("Fire2"))
+        {
+            var raycastHit = Physics2D.Raycast(transform.position, new Vector2(transform.position.x, transform.position.y - 1));
+            var go = raycastHit.collider.gameObject.GetComponent<TetrominosBehaviour>();
+
+            if (go != null)
+            {
+                go.SnapTetrominoToPlace();
+            }
+        }
+
+
         targetVelocityX = IncrementTowards(targetVelocityX, MoveSpeed *  horizontal, Acceleration, dt);
 
         //targetVelocityY = IncrementTowards(targetVelocityY, MoveSpeed * vertical, Acceleration, dt);
